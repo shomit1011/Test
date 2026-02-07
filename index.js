f6b34adb6fc37ecf568630fca67d89b5977f1742
shomit1011 <shomitbhattacharya042@gmail.com>
2026-02-07 21:35:02 +0530

    Added Validations

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-// const cards = document.querySelectorAll(".day-card");
-// const modal = document.getElementById("loveModal");
-// const closeModal = document.getElementById("closeModal");
+const cards = document.querySelectorAll(".day-card");
+const modal = document.getElementById("loveModal");
+const closeModal = document.getElementById("closeModal");
 
-// const today = new Date();
-// const currentDay = today.getDate();
-// const currentMonth = today.getMonth(); // Feb = 1
+const today = new Date();
+const currentDay = today.getDate();
+const currentMonth = today.getMonth(); // Feb = 1
 
-// cards.forEach(card => {
-//   const unlockDay = parseInt(card.dataset.day);
-//   const unlockMonth = parseInt(card.dataset.month);
+cards.forEach(card => {
+  const unlockDay = parseInt(card.dataset.day);
+  const unlockMonth = parseInt(card.dataset.month);
 
-//   const isUnlocked =
-//     currentMonth > unlockMonth ||
-//     (currentMonth === unlockMonth && currentDay >= unlockDay);
+  const isUnlocked =
+    currentMonth > unlockMonth ||
+    (currentMonth === unlockMonth && currentDay >= unlockDay);
 
-//   if (!isUnlocked) {
-//     card.classList.add("locked");
+  if (!isUnlocked) {
+    card.classList.add("locked");
 
-//     card.addEventListener("click", (e) => {
-//       e.preventDefault();
-//       modal.style.display = "flex";
-//     });
-//   }
-// });
+    card.addEventListener("click", (e) => {
+      e.preventDefault();
+      modal.style.display = "flex";
+    });
+  }
+});
 
-// closeModal.addEventListener("click", () => {
-//   modal.style.display = "none";
-// });
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
-// window.addEventListener("click", (e) => {
-//   if (e.target === modal) {
-//     modal.style.display = "none";
-//   }
-// });
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
 
 const heartsContainer = document.getElementById("hearts");
 
